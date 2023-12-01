eda29cfb6bf41d972227a579e9fc8ee8ebe847b0
firdayulianti <98600818+firdayulianti@users.noreply.github.com>
2023-12-01 13:31:12 +0700

    Add files via upload

---
+++ materi.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Qursuo</title>
+    <!-- Bulma Version 0.9.4-->
+    <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.4/css/bulma.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.2/tailwind.min.css">
+    <link rel="stylesheet" type="text/css" href="showcase.css" />
+    <script
+      src="https://kit.fontawesome.com/2828f7885a.js"
+      integrity="sha384-WAsFbnLEQcpCk8lM1UTWesAf5rGTCvb2Y+8LvyjAAcxK1c3s5c0L+SYOgxvc6PWG"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="icon" type="image/png" href="favicon.png" />
+  </head>
+
+  <body>
+    <!-- Begin Preloader -->
+    <div class="preloader-wrapper">
+      <div class="preloader">
+        <img src="img/preloader.gif" alt="" />
+      </div>
+    </div>
+    <!-- End Preloader-->
+    <!-- Begin Scroll Up Button -->
+
+    <form action="#home">
+      <button id="toTop" title="Go to top">
+        <i class="fas fa-angle-up"></i>
+      </button>
+    </form>
+    <!-- End Scroll Up Button -->
+
+    <!-- Begin Header -->
+    <div class="header-wrapper" id="home">
+      <!-- Begin Hero -->
+      <section class="hero is-large">
+        <!-- Begin Mobile Nav -->
+        <nav class="navbar is-fixed-top is-transparent is-hidden-desktop">
+          <!-- Begin Burger Menu -->
+          <div class="navbar-brand">
+            <div class="navbar-burger burger" data-target="mobile-nav">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <!-- End Burger Menu -->
+          <div id="mobile-nav" class="navbar-menu">
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <a class="navbar-item" href="#home">
+                  Home
+                </a>
+              </div>
+              <div class="navbar-item">
+                <a class="navbar-item" href="#about-me">
+                  About Us
+              <div class="navbar-item">
+                <a class="navbar-item" href="#contact">
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <!-- End Mobile Nav -->
+        <!-- Begin Hero Content-->
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="subtitle">This Is My Qursuo</h1>
+            <h2 class="title">Qursuo</h2>
+            <h1 class="subtitle profession">welcome to my Proposal Kursus Online</h1>
+          </div>
+        </div>
+        <!-- End Hero Content-->
+        <!-- Begin Hero Menu -->
+        <div class="hero-foot ">
+          <div class="hero-foot--wrapper">
+            <div class="columns">
+              <div class="column is-12 hero-menu-desktop has-text-centered is-fixed-top">
+                <ul>
+                  <li class="is-active">
+                    <a href="#home">Home</a>
+                  </li>
+                  <li>
+                    <a href="#about-me">Proposal</a>
+                  </li>
+                  <li>
+                    <a href="#contact">About Us</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Hero Menu -->
+      </section>
+      <!-- End Hero -->
+    </div>
+    <!-- End Header -->
+
+    <!-- Begin Main Content -->
+    <div class="main-content">
+      <!-- Begin About Me Section -->
+      <div class="section-light about-me" id="about-me">
+        <div class="container">
+          <div class="column is-12 about-me">
+            <h1 class="title has-text-centered section-title">Proposal</h1>
+          </div>
+          <div class="columns is-multiline">
+            <div
+              class="column is-6 has-vertically-aligned-content"
+              data-aos="fade-right"
+            >
+              <p class="is-larger">
+                &emsp;&emsp;<strong
+                  >Project 3</strong>
+              </p>
+              <br />
+              <p>
+                Aplikasi Rekomendasi Kursus Online adalah teman terpercaya dalam perjalanan pendidikan dan pengembangan diri Anda. Dengan akses ke berbagai mata pelajaran dan topik, aplikasi ini dirancang untuk membantu Anda mencapai tujuan belajar Anda dengan cara yang paling efektif dan efisien.
+              </p>
+              <br />
+              <a href="proposal.html" class="button is-primary">Baca Selengkapnya</a>
+
+              <div class="is-divider"></div>
+              <div class="columns about-links">
+                <div class="column">
+                  <p class="heading">
+                    <strong>call Us</strong>
+                  </p>
+                  <p class="subheading">
+                    1204058 & 1214058
+                  </p>
+                </div>
+                <div class="column">
+                  <p class="heading">
+                    <strong>Email us</strong>
+                  </p>
+                  <p class="subheading">
+                    Qursuo458@gmail.com
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="column is-6 right-image " data-aos="fade-left">
+              <img
+                class="is-rounded"
+                src="img/kursus.jpeg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Work Content -->
+      <!-- Begin Contact Content -->
+      <div class="section-light contact" id="contact">
+        <div class="container">
+          <div
+            class="columns is-multiline"
+            data-aos="fade-in-up"
+            data-aos-easing="linear"
+          >
+            <div class="column is-12 about-me">
+              <h1 class="title has-text-centered section-title">
+                    <div id="ibrohim"
+        class="flex items-center justify-center w-full px-8 py-10 border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0">
+        <div class="max-w-6xl mx-auto">
+          <div class="flex-col items-center ">
+              <div class="flex flex-col items-center justify-center w-full h-full max-w-2xl pr-8 mx-auto text-center">
+                  <p class="my-5 text-base font-medium tracking-tight text-indigo-500 uppercase">Profil Singkat 
+                  </p>
+                  <h2
+                      class="text-4xl font-extrabold leading-10 tracking-tight text-gray-900 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">
+                      Bimo Arga Dewantoro</h2>
+    
+    
+              </div>
+              <div class="flex flex-col items-center justify-center max-w-2xl py-8 mx-auto xl:flex-row xl:max-w-full">
+                  <div class="w-full xl:w-1/2 xl:pr-8">
+                      <blockquote
+                          class="flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-white hover:shadow ease">
+                          <div class="flex flex-col pr-8">
+                              <div class="relative pl-12">
+                                  <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+                                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
+                                      <path
+                                          d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z" />
+                                  </svg>
+                                  <p class="mt-2 text-base text-gray-600">Mahasiswa semester 5 di Universitas Logistik Bisnis Internasional Fakultas Vokasi Prodi Teknik Informatika Kelas 3C
+                                  </p>
+                              </div>
+    
+                              <h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 truncate">Bimo Arga Dewantoro <span class="mt-1 text-sm leading-5 text-gray-500 truncate">- ULBI</span></h3>
+                              <p class="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
+                          </div>
+                          <img class="flex-shrink-0 object-cover w-24 h-24 mb-5 bg-gray-300 rounded-full md:mb-0"
+                              src="img/bimo.jpeg"
+                              alt="">
+                      </blockquote>
+                  </div>
+              </div>
+              
+              <div class="column is-12 about-me">
+                <h1 class="title has-text-centered section-title">
+                      <div id="ibrohim"
+          class="flex items-center justify-center w-full px-8 py-10 border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0">
+          <div class="max-w-6xl mx-auto">
+            <div class="flex-col items-center ">
+                <div class="flex flex-col items-center justify-center w-full h-full max-w-2xl pr-8 mx-auto text-center">
+                    <p class="my-5 text-base font-medium tracking-tight text-indigo-500 uppercase">Profil Singkat 
+                    </p>
+                    <h2
+                        class="text-4xl font-extrabold leading-10 tracking-tight text-gray-900 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">
+                        Firda Yulianti</h2>
+      
+      
+                </div>
+                <div class="flex flex-col items-center justify-center max-w-2xl py-8 mx-auto xl:flex-row xl:max-w-full">
+                    <div class="w-full xl:w-1/2 xl:pr-8">
+                        <blockquote
+                            class="flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-white hover:shadow ease">
+                            <div class="flex flex-col pr-8">
+                                <div class="relative pl-12">
+                                    <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+                                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
+                                        <path
+                                            d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z" />
+                                    </svg>
+                                    <p class="mt-2 text-base text-gray-600">Mahasiswa semester 5 di Universitas Logistik Bisnis Internasional Fakultas Vokasi Prodi Teknik Informatika Kelas 3C
+                                    </p>
+                                </div>
+      
+                                <h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 truncate">Firda Yulianti <span class="mt-1 text-sm leading-5 text-gray-500 truncate">- ULBI</span></h3>
+                                <p class="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
+                            </div>
+                            <img class="flex-shrink-0 object-cover w-24 h-24 mb-5 bg-gray-300 rounded-full md:mb-0"
+                                src="img/nda.jpeg"
+                                alt="">
+                        </blockquote>
+                    </div>
+                </div>
+          </div>
+      </div>
+    </div>
+    <!-- End Testimonials-->
+
+    <!-- Start Testimonials -->
+    <div id="akbar"
+        class="flex items-center justify-center w-full px-8 py-10 border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0">
+        
+    </div>
+              </h1>
+            </div>
+            <div class="column is-8 is-offset-2">
+              <form
+                action="https://formspree.io/email@example.com"
+                method="POST"
+              >
+
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Contact Content -->
+    </div>
+    <!-- End Main Content -->
+
+    <!-- Begin Footer -->
+    <!-- End Footer -->
+
+    <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="showcase.js"></script>
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+    <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+    <script>
+      AOS.init({
+        easing: "ease-out",
+        duration: 800,
+      });
+    </script>
+  </body>
+</html>
